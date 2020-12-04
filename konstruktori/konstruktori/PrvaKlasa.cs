a2a104b9b6e0ee0c724de960d3bf2214208d2a11
RobertJanjanin <55134583+RobertJanjanin@users.noreply.github.com>
2020-12-04 15:14:00 +0100

    Add files via upload

---
+++ konstruktori/konstruktori/PrvaKlasa.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace konstruktori
+{
+    class PrvaKlasa
+    {
+       
+        string ime;
+
+        string prezime;
+
+        static int brojac;
+
+       public override void ToString()
+
+        {
+
+         Console.WriteLine("Redni broj: " +brojac+ "," + ime+prezime);
+
+                                                                          }
+
+      public PrvaKlasa()
+      {
+        brojac++;
+                     }
+       
+        public static int Counter { get => brojac; }
+
+        public string Ime { get => ime; set => ime = value; }
+
+        public string Prezime { get => prezime; set => prezime = value; }
+
+    }
+}
